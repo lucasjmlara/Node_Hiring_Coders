cb9b40fe29f9e043aa01fced81e84b8f5b7b09ac
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-06-05 16:42:04 -0300

    Aula programa basico nodejs

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+
+  const params = queryString.parse(url.parse(req.url, true).search)
+  const pergunta = params.pergunta;
+  let resposta;
+
+  if(pergunta == 'melhor-filme') {
+    resposta = 'Duna';
+  } else if (pergunta == 'melhor-tech-backend') {
+    resposta = 'nodejs';
+  } else {
+    resposta = 'nao sei, sorry!';
+  }
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(resposta);
 });
 
 server.listen(port, hostname, () => {
